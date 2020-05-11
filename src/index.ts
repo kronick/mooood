18d3999af9db94963ca0d2c02d3969bc4b47b92c
Sam Kronick <sam.kronick@gmail.com>
2020-05-10 22:18:42 -0700

    Parse message

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -6,7 +7,13 @@ const app = express();
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
-app.post("/incoming", (req, res) => {
-  console.log("Incoming!", req.body);
-  res.send("OK");
-});
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.post(
+  "/incoming",
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    const message = req.body.Body;
+    console.log("Incoming!", message);
+    res.send("OK");
+  }
+);
